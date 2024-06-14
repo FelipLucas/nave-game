a989f5ea48294a2fb71716ca0138aab033f643b8
Felipe <fefelbf@gmail.com>
2024-06-14 11:34:04 -0300

    comir

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,10 @@
 const timer = document.querySelector('.time p');
 const nave = document.querySelector('.nave');
+const navePosition = nave.getBoundingClientRect();
 const widthNave = nave.offsetWidth;
 const heightNave = nave.offsetHeight;
 const naveInimiga = document.querySelector('.nave-inimiga')
+const naveInimigaPosition = naveInimiga.getBoundingClientRect()
 const cenario = document.querySelector('.nave-game');
 const widthCenario = cenario.offsetWidth;
 const heightCenario = cenario.offsetHeight;
@@ -14,6 +16,8 @@ let positionY = 0
 let directionX = 0;
 let directionY = 0;
 let velocidade = 15;
+let velocidadeTiro = 20;
+let estaAtirando = false;
 
 audioMusica = new Audio('sounds/missaoespaco.mp3');
 
@@ -24,6 +28,8 @@ const startGame = () =>{
     document.addEventListener('keydown', keyIsPressed);
     document.addEventListener('keyup', keyStopped);
     setInterval(refreshMove, 50);
+    setInterval(atirar, 10);
+    setInterval(moveTiros, 50);
 
     audioMusica.play()
     setTimeout(() =>{
@@ -90,3 +96,61 @@ function refreshMove(){
         positionY = heightCenario - heightNave;
     }
 }
+
+//atirar
+const criarTiros = (positionLeftTiro, positionTopTiro) =>{
+    const tiro = document.createElement("div");
+    tiro.style.position = 'absolute';
+    tiro.style.width = '10px';
+    tiro.style.height = '10px';
+    tiro.style.borderRadius = '50%';
+    tiro.style.background = 'red';
+    tiro.style.left = positionLeftTiro + 'px';
+    tiro.style.top = positionTopTiro + 'px';
+    cenario.appendChild(tiro);
+}
+
+const atirar = () =>{
+    if(estaAtirando){
+        criarTiros(positionX + 45, positionY  - 10);
+    }
+}
+
+const moveTiros = () =>{
+    const tiros = document.querySelectorAll('.tiro');
+    for(let i = 0; i < tiros.length; i++){
+        if(tiros[1]){
+            let positionTopTiro = tiros[i].offsetTop;
+            positionTopTiro -= velocidadeTiro;
+            tiros[i].style.top = positionTopTiro + 'px';
+        }
+    }
+}
+
+document.addEventListener("keydown", (event) =>{
+    if(event.key === " "){
+        atirar();
+        estaAtirando = true;
+    }
+})
+document.addEventListener("keyup", (event) =>{
+    if(event.key === " "){
+        estaAtirando = false;
+    }
+})
+
+//colisão nave inimiga
+const colisaoNave = setInterval(() =>{
+    if(naveInimigaPosition.bottm > navePosition.top && navePosition.top < naveInimigaPosition.bottom){
+        directionX = 0;
+        directionY = 0;
+
+        naveInimiga.style.animation = 'none';
+
+        nave.style.background = url("images/explosao.gif");
+
+        audioMusica.pause();
+
+        clearInterval(timer, 10);
+    }
+},50);
